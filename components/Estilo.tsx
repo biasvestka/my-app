bb48b069101cac6d72d63cb066047d9ad0ce82b8
biasvestka <biasvestka@gmail.com>
2024-03-05 11:05:45 -0300

    estilização

--- components/Estilo.tsx
+++ components/Estilo.tsx
@@ -1,79 +1,85 @@
 const Estilo = {
   appContainer: {
     flex: 1,
-    backgroundColor: 'black', // Fundo preto
+    backgroundColor: '#000', // Fundo preto
   },
   viewSafeAndroid: {
     flex: 1,
     padding: 20,
-    backgroundColor: 'black',
+    backgroundColor: '#000', // Fundo preto
   },
   viewImage: {
     alignItems: 'center',
     marginTop: 30,
     marginBottom: 30,
-    borderWidth: 1,
-    borderColor: 'gray',
     padding: 20,
   },
   tamImage: {
-    width: 200,
-    height: 200,
+    width: 100, // Diminuindo o tamanho da imagem para 150
+    height: 100, // Diminuindo o tamanho da imagem para 150
   },
   todoSection: {
     marginBottom: 30,
   },
   todoTitle: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
-    color: 'white', // Texto branco
+    color: '#fff', // Texto branco
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 20,
+    backgroundColor: '#222', // Cinza escuro
+    borderRadius: 8,
+    paddingHorizontal: 10,
   },
   input: {
     flex: 1,
     height: 40,
-    borderWidth: 1,
-    borderColor: 'gray',
-    paddingHorizontal: 10,
-    color: 'white', // Texto branco
+    color: '#fff', // Texto branco
   },
   addButton: {
     backgroundColor: '#007bff',
-    padding: 10,
-    marginLeft: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginLeft: 10,
   },
   addButtonText: {
-    color: 'white', // Texto branco
+    fontSize: 16,
+    color: '#fff', // Texto branco
   },
   todoItem: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 10,
+    backgroundColor: '#333', // Cinza mais escuro
+    padding: 10,
+    borderRadius: 8,
   },
   todoText: {
     flex: 1,
-    color: 'white', // Texto branco
+    color: '#fff', // Texto branco
   },
   deleteButton: {
     backgroundColor: '#dc3545',
     padding: 5,
     marginRight: 10,
+    borderRadius: 5,
   },
   deleteButtonText: {
-    color: 'white', // Texto branco
+    color: '#fff', // Texto branco
   },
   completeButton: {
-    backgroundColor: 'green',
+    backgroundColor: '#28a745',
     padding: 5,
     marginRight: 10,
+    borderRadius: 5,
   },
   completeButtonText: {
-    color: 'white', // Texto branco
+    color: '#fff', // Texto branco
   },
 };
 
